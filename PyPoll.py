7ab9cfb18dbab515f9da462fd2af0ff9412e5a20
Jack Key <jhschulte44@gmail.com>
2021-09-22 20:51:32 -0400

    Updating results text file

--- PyPoll.py
+++ PyPoll.py
@@ -20,10 +20,10 @@ winning_percentage = 0
 # Open election results and read file
 with open(file_to_load) as election_data:
 
-    # to-do: read and analyze data here
+    # read and analyze data here
     file_reader = csv.reader(election_data)
 
-    # read and print header
+    # read header
     headers = next(file_reader)
     
     # print each row in the CSV file
@@ -36,15 +36,21 @@ with open(file_to_load) as election_data:
         if candidate_name not in candidate_options:
             # Add candidate name from each row
             candidate_options.append(candidate_name)
-
             # Begin tracking that candidate's vote count
             candidate_votes[candidate_name] = 0
-
         # Add a vote to candidates count
         candidate_votes[candidate_name] += 1 
-        
-    
 
+    # Write results into text file
+    with open(file_to_save, "w") as txt_file:
+        election_results = (
+            f"\nElection Results\n"
+            f"--------------------\n"
+            f"Total Votes: {total_votes:,}\n"
+            f"--------------------\n")
+        print(election_results, end="")
+        txt_file.write(election_results)
+            
 # Determine percentages of votes for each candidate
 # Iterate through list of candidates
 for candidate_name in candidate_votes:
@@ -54,7 +60,7 @@ for candidate_name in candidate_votes:
     vote_percentage = float(votes) / float(total_votes) * 100
 
     # Print out each candidates name, vote count, and percentage of total vote
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
      # Determine if vote count is greater than winning count
     if (votes > winning_count) and (vote_percentage > winning_percentage):
@@ -71,19 +77,7 @@ winning_candidate_summary = (
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
-print(winning_candidate_summary)
+#print(winning_candidate_summary)
     
 
 
-
-
-with open(file_to_save, "w") as txt_file:
-    txt_file.write("Counties in the Election\nArapahoe\nDenver\nJefferson")
-
-
-
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote
